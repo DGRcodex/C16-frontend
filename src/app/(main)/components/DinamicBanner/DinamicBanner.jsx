83ecef61f131841087cec77d2294cbeaab65ac8a
Daniel Garcia Rojas <dgrcodex@MacBook-Pro-de-Pedro.local>
2024-04-16 15:18:07 -0400

    se crean versiones de solucion al fix

--- src/app/(main)/components/DinamicBanner/DinamicBanner.jsx
+++ src/app/(main)/components/DinamicBanner/DinamicBanner.jsx
@@ -5,11 +5,17 @@ function DinamicBanner({ imageSrc, title, message, customStyles }) {
     return (
         <div className={`${styles.desktopContainer} ${customStyles.desktopContainer}`}>
             <section className={`${styles.banner} ${customStyles.banner}`} role='banner'>
-                <Image
-                 width={150}
-                  height={150} 
-                  alt='banner image'
-                   src={imageSrc} className={`${styles.bannerImg} ${customStyles.bannerImg}`} />
+            <Image
+  width={100}
+  height={100}
+  alt='banner image'
+  src={imageSrc}
+  layout='responsive' // Asegura que la imagen sea responsive
+  objectFit='cover' // Mantiene la relación de aspecto y cubre el área del contenedor
+  className={`${styles.bannerImg} ${customStyles.bannerImg}`}
+/>
+
+
             </section>
 
             <div className={`${styles.bannerTextBox} ${customStyles.bannerTextBox}`}>
